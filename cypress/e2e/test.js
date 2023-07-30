935215d7c0a558389cce1b0709283a02da154ceb
asaqa988 <asaqa001@gmail.com>
2023-07-30 20:53:50 +0300

    update until 7

--- cypress/e2e/test.js
+++ cypress/e2e/test.js
@@ -56,7 +56,7 @@ describe("testjs", () => {
 
     });
 
-    it('randomly change the language of the website and create one assertion for each change ', () => {
+    it.only('randomly change the language of the website and create one assertion for each change ', () => {
 
 
 
@@ -73,29 +73,94 @@ describe("testjs", () => {
 
         cy.url().then((url) => {
 
+            let ArabicCities = ["جدة","دبي"]
+            let randomArabic = Math.floor(Math.random()*ArabicCities.length)
+            let englishCities = ["dubai","jeddah","riyadh"]
+            let randomEnglish = Math.floor(Math.random()*englishCities.length)
+
+            let roomVisitor = ["A","B"]
+
+            let randomvistorNo = Math.floor(Math.random()*roomVisitor.length)
+
+let lowestPrice = ""
+
+let HighestPrice = ""
 
             if (url.includes('ar')) {
 
 
                 //hard way 
-                cy.get('.sc-gRnDUn').should('have.attr','src').and('include',"ar")
+                cy.get('.sc-gRnDUn').should('have.attr', 'src').and('include', "ar")
 
 
                 // easy way 
                 cy.get('[data-testid="Header__LanguageSwitch"]').should('contain', "English")
+
+
+                cy.get('#uncontrolled-tab-example-tab-hotels').click()
+                cy.get('[data-testid="AutoCompleteInput"]').type(ArabicCities[randomArabic])
+                cy.get('[data-testid="AutoCompleteResultItem0"] > .sc-12clos8-5').click()
+                cy.get('[data-testid="HotelSearchBox__SearchButton"]').click()
+                cy.get('select[data-testid="HotelSearchBox__ReservationSelect_Select"]').select(roomVisitor[randomvistorNo])
+                cy.get('[data-testid="HotelSearchBox__SearchButton"]')
+
+                
+                // cy.document().its('readyState').should('eq', 'complete');
+
+                // point 8 last one 
+
+                cy.get('[data-testid="HotelSearchResult__sort__LOWEST_PRICE"]').click()
+
+                cy.get('[data-testid="HotelSearchResult__Hotel0__PriceLabel"] > .Price__Value').then((element)=>{
+                    
+                    lowestPrice=element.text()
+                })
+
+
+                
+                cy.get('[data-testid="HotelSearchResult__Hotel39__PriceLabel"]').find("Price__Value").then((element)=>{
+
+                    HighestPrice=element.text()
+                    console.log(HighestPrice)
+
+                })
 
             } else if (url.includes('en')) {
 
 
 
                 //hard way 
-                cy.get('.sc-gRnDUn').should('have.attr','src').and('include',"en")
+                cy.get('.sc-gRnDUn').should('have.attr', 'src').and('include', "en")
 
 
                 // easy way 
                 cy.get('[data-testid="Header__LanguageSwitch"]').should('contain', "العربية")
 
+                cy.get('#uncontrolled-tab-example-tab-hotels').click()
 
+                cy.get('[data-testid="AutoCompleteInput"]').type(englishCities[randomEnglish])
+                cy.get('[data-testid="AutoCompleteResultItem0"] > .sc-12clos8-5').click()
+                cy.get('[data-testid="HotelSearchBox__SearchButton"]').click()
+                cy.get('select[data-testid="HotelSearchBox__ReservationSelect_Select"]').select(roomVisitor[randomvistorNo])
+                cy.get('[data-testid="HotelSearchBox__SearchButton"]')
+
+                // cy.document().trigger('pageLoaded');
+
+                // point 8 last one 
+
+                cy.get('[data-testid="HotelSearchResult__sort__LOWEST_PRICE"]').click()
+
+                cy.get('[data-testid="HotelSearchResult__Hotel0__PriceLabel"] > .Price__Value').then((element)=>{
+                    
+                    lowestPrice=element.text()
+                })
+
+                cy.get('[data-testid="HotelSearchResult__Hotel39__PriceLabel"]').find("Price__Value").then((element)=>{
+
+                    HighestPrice=element.text()
+                    console.log(HighestPrice)
+
+                })
 
             }
 
@@ -103,7 +168,10 @@ describe("testjs", () => {
 
 
 
-    });
+    }
+    
+  );
+
 }
 
 
